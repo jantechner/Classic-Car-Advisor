cbae51704e1cf13c08198e4ad6712be733363e66
Jan Techner <jantechner@live.com>
2018-11-29 09:49:20 +0100

    Add JavaFX skeleton - NOT WORKING

--- src/main/java/com/drools/example/CarAdvisor.java
+++ src/main/java/com/drools/example/CarAdvisor.java
@@ -1,8 +1,20 @@
 package com.drools.example;
 
-public class CarAdvisor {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class CarAdvisor extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+      /*
+      Code for JavaFX application.
+      (Stage, scene, scene graph)
+      */
+    }
+
+    public static void main(String args[]){
+        launch(args);
     }
 
 }
---
+++ src/main/java/com/drools/example/controllers/StartViewContrroller.java
@@ -0,0 +1,8 @@
+package com.drools.example.controllers;
+
+public class StartViewContrroller {
+
+    public void runAdvisor() {
+        System.out.println("press start button");
+    }
+}
