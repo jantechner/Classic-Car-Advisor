2883907a73797a020157f27f717749ac61895978
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2018-12-05 20:09:34 +0100

    QuestionViewController - fxml references

--- src/main/java/com/drools/example/controllers/QuestionViewController.java
+++ src/main/java/com/drools/example/controllers/QuestionViewController.java
@@ -4,31 +4,31 @@ import com.drools.example.Question;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 
 public class QuestionViewController {
 
-//    private Question question;
-//
+    private Question question;
+
 //    public QuestionViewController(Question question) {
 //        this.question = question;
 //    }
-//
-//    @FXML
-//    private Label questionContent;
-//
+
+    @FXML
+    private Label questionContent;
+
+    @FXML
+    private VBox answerBox, resultBox;
+
 //    @FXML
 //    public void initialize() {
 //        questionContent.setText(question.content);
 //    }
-//
-//    @FXML
-//    public void userAnswer(ActionEvent event) {
-//
-//    }
-//
 
+    @FXML
+    public void userAnswer(ActionEvent event) {
 
-
+    }
 
 }
