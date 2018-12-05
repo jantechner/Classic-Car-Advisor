f39838a7f18e37d30b02a789f0f8e120d29f6c3b
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2018-12-05 19:56:49 +0100

    FXML cleanup

--- src/main/java/com/drools/example/controllers/QuestionViewController.java
+++ src/main/java/com/drools/example/controllers/QuestionViewController.java
@@ -3,29 +3,30 @@ package com.drools.example.controllers;
 import com.drools.example.Question;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+
 
 public class QuestionViewController {
 
-    private Question question;
-
-    public QuestionViewController(Question question) {
-        this.question = question;
-    }
-
-    @FXML
-    private Text questionContent;
-
-    @FXML
-    public void initialize() {
-        questionContent.setText(question.content);
-    }
-
-    @FXML
-    public void userAnswer(ActionEvent event) {
-
-    }
-
+//    private Question question;
+//
+//    public QuestionViewController(Question question) {
+//        this.question = question;
+//    }
+//
+//    @FXML
+//    private Label questionContent;
+//
+//    @FXML
+//    public void initialize() {
+//        questionContent.setText(question.content);
+//    }
+//
+//    @FXML
+//    public void userAnswer(ActionEvent event) {
+//
+//    }
+//
 
 
 
--- src/main/java/com/drools/example/controllers/StartViewController.java
+++ src/main/java/com/drools/example/controllers/StartViewController.java
@@ -1,13 +1,35 @@
 package com.drools.example.controllers;
 
 import com.drools.example.Session;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartViewController {
+
+    @FXML
+    private AnchorPane pane;
 
     public void runAdvisor() {
 
         System.out.println("Start button pressed");
         Session.session.fireAllRules();
 
+        Stage stage = (Stage) pane.getScene().getWindow();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/questionView.fxml"));
+            pane = loader.load();
+            Scene scene = new Scene(pane);
+            stage.setScene(scene);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
